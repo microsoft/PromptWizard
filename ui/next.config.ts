55d92842c2050dad84b922fcd1a93e3aaa53109d
Vansh Oberoi <learnsolo462@gmail.com>
2025-05-13 20:46:12 +0530

    netlify
    vercel went 250mb+ so trying netlify

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -3,14 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: process.env.NODE_ENV === 'development'
-          ? 'http://localhost:5000/api/:path*'
-          : '/api/:path*',
-      },
-    ];
+    return [];
   },
 };
 
