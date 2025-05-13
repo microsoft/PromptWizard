3855f0a563cbd75ba849e321e7c8271eef99e230
Vansh Oberoi <learnsolo462@gmail.com>
2025-05-13 21:05:08 +0530

    render for api

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
-    return [];
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NODE_ENV === 'development'
+          ? 'http://localhost:5000/api/:path*'
+          : 'https://<render-api-url>/api/:path*',
+      },
+    ];
   },
 };
 
